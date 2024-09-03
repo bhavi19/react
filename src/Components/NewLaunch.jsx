faf21b5c631fa2cb52295bb2f50e6c2522be3143
gopal kumar <ambitiousbhavi@gmail.com>
2024-09-03 23:44:58 +0530

    homepage:initial commit

---
+++ src/Components/NewLaunch.jsx
@@ -0,0 +1,11 @@
+
+import Slider from './Carosuel';
+
+const NewLaunch = () => {
+    return (
+        <div style={{ height: '300px' }}>
+            <Slider />
+        </div>
+    )
+}
+export default NewLaunch;
---
+++ src/Components/VideoSlider.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const VideoSlider = () => {
+    return (
+        <Container>
+            <video autoPlay loop muted id="video">
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+            </video>
+        </Container>
+    )
+}
+
+export default VideoSlider;
